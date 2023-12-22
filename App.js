b5c0663232d5679c58d754b970371447748a74b4
Augusto Chaves <jose.rangel.ext@fibrasil.com.br>
2023-12-22 14:57:36 -0300

    Criando a tela de Cesta

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
-import { SafeAreaView, StatusBar } from 'react-native';
+import { SafeAreaView, StatusBar, View } from 'react-native';
 import Cesta from './src/pages/Cesta';
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_400Regular_Italic,
+  Montserrat_700Bold
+} from '@expo-google-fonts/montserrat';
 
 export default function App() {
+  const [fonte] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold
+  });
+
+  if (!fonte){
+    return <View />
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
--- src/pages/Cesta.js
+++ src/pages/Cesta.js
@@ -1,13 +1,23 @@
 import React from "react";
-import { StyleSheet, Image, Dimensions, Text } from "react-native";
+import { StyleSheet, Image, Dimensions, Text, View } from "react-native";
 
 import topo from '../../assets/topo.png';
+import logo from '../../assets/logo.png';
 const width = Dimensions.get('screen').width;
 
 export default function Cesta() {
     return <>
         <Image source={topo} style={estilos.topo} />
         <Text style={estilos.titulo}>Detalhe da Cesta</Text>
+        <View style={estilos.cesta}>
+            <Text style={estilos.subtitulo}>Cesta de Verduras</Text>
+            <view style={estilos.vFabricante}>
+                <Image source={logo} style={estilos.imagemLogo} />
+                <Text style={estilos.fabricante}>Jenny Jack Farm</Text>
+            </view>
+            <Text style={estilos.descricao}>Uma cesta com produtos selecionados cuidadozamente da fazenda direto para a sua cozinha</Text>
+            <Text style={estilos.preco}>R$ 40,00</Text>
+        </View>
     </>
 }
 
@@ -25,5 +35,40 @@ const estilos = StyleSheet.create({
         color: "white",
         fontWeight: "bold",
         padding: 16
+    },
+    cesta: {
+        paddingHorizontal: 16,
+        paddingVertical: 8
+    },
+    subtitulo: {
+        fontSize: 26,
+        lineHeight: 42,
+        color: "#464646",
+        fontWeight: "bold"
+    },
+    vFabricante: {
+        flexDirection: "row",
+        paddingVertical: 12
+    },
+    imagemLogo: {
+        width: 32,
+        height: 32
+    },
+    fabricante: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 12
+    },
+    descricao: {
+        color: "#A3A3A3",
+        fontSize: 16,
+        lineHeight: 26
+    },
+    preco: {
+        color: "#2A9F85",
+        fontWeight: "bold",
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8
     }
 })
